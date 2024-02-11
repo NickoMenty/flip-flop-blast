4f7663f0d1e5156c1183faa66b06b3db239e729c
Jr Buchynskyi <jrbuchynskyi@MacBook-Pro-Jr.local>
2024-02-11 23:09:29 +0100

    added rel

--- components/Footer.js
+++ components/Footer.js
@@ -6,10 +6,10 @@ export default function Footer() {
         <div className="p-5 footer">
             <img className="footer-logo" src="/img/flipflop_logo.png"></img>
             <div className="footer-links">
-                <a className="footer-link" href="https://flip-flop.gitbook.io/ffdocs/" target="_blank">Whitepaper</a>
-                <a className="footer-link" href="https://flip-flop.gitbook.io/ffdocs/roadmap-and-plans" target="_blank">Roadmap</a>
-                <a className="footer-link" href="https://twitter.com/ffonblast" target="_blank"><FaXTwitter /></a>
-                <a className="footer-link" href="https://discord.gg/DTqzYtfDwQ" target="_blank"><FaDiscord /></a>
+                <a className="footer-link" href="https://flip-flop.gitbook.io/ffdocs/" target="_blank" rel="noreferrer">Whitepaper</a>
+                <a className="footer-link" href="https://flip-flop.gitbook.io/ffdocs/roadmap-and-plans" target="_blank" rel="noreferrer">Roadmap</a>
+                <a className="footer-link" href="https://twitter.com/ffonblast" target="_blank" rel="noreferrer"><FaXTwitter /></a>
+                <a className="footer-link" href="https://discord.gg/DTqzYtfDwQ" target="_blank" rel="noreferrer"><FaDiscord /></a>
             </div>
         </div>
     )
