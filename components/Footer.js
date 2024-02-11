d2391d763a7e1abe84b1a092469f559d19da848e
SashaFeyaa <sashulya02@gmail.com>
2024-02-11 18:18:04 +0200

    Links updated target

--- components/Footer.js
+++ components/Footer.js
@@ -6,10 +6,10 @@ export default function Footer() {
         <div className="p-5 footer">
             <img className="footer-logo" src="/img/flipflop_logo.png"></img>
             <div className="footer-links">
-                <a className="footer-link" href="https://flip-flop.gitbook.io/ffdocs/">Whitepaper</a>
-                <a className="footer-link" href="https://flip-flop.gitbook.io/ffdocs/roadmap-and-plans">Roadmap</a>
-                <a className="footer-link" href="https://twitter.com/ffonblast"><FaXTwitter /></a>
-                <a className="footer-link" href="https://discord.gg/DTqzYtfDwQ"><FaDiscord /></a>
+                <a className="footer-link" href="https://flip-flop.gitbook.io/ffdocs/" target="_blank">Whitepaper</a>
+                <a className="footer-link" href="https://flip-flop.gitbook.io/ffdocs/roadmap-and-plans" target="_blank">Roadmap</a>
+                <a className="footer-link" href="https://twitter.com/ffonblast" target="_blank"><FaXTwitter /></a>
+                <a className="footer-link" href="https://discord.gg/DTqzYtfDwQ" target="_blank"><FaDiscord /></a>
             </div>
         </div>
     )
